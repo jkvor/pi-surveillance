37b3b845915ff71100d37d3415bc4ff17a665ea8
Jacob Vorreuter <jacob.vorreuter@gmail.com>
2013-10-31 04:58:45 +0000

    add upstart script

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 http://learn.adafruit.com/system/assets/assets/000/003/929/original/breadboard.png?1360755758
 
 ```
+$ sudo apt-get install upstart
 $ sudo apt-get install python-dev
 $ sudo apt-get install python-rpi.gpio
+$ sudo cp motion-detector.conf /etc/init/
+$ sudo start motion-detector
 ```
--- motion-detector.py
+++ motion-detector.py
@@ -1,6 +1,9 @@
-import time, os
+#!/usr/bin/python
+
+import time, os, sys
 import RPi.GPIO as io
 io.setmode(io.BCM)
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) # reopen stdout in unbuffered mode
 
 pir_pin = 18
 io.setup(pir_pin, io.IN) # activate input
@@ -9,6 +12,7 @@ io.setup(pir_pin, io.IN) # activate input
 throttle_window = 10
 last_activity = 0
 
+print "starting loop"
 while True:
     if (int(time.time()) - last_activity) > throttle_window:
         if io.input(pir_pin):
